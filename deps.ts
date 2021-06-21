ccbbf4d44dc8f853752bf323cece8983e1b88de1
Julli4n <11952329+Julli4n@users.noreply.github.com>
2021-06-21 02:32:25 -0700

    use versioned deps

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/x/evt/mod.ts";
-export { default as WS } from "https://raw.githubusercontent.com/Scientific-Guy/custom-socket/main/mod.ts";
+export * from "https://deno.land/x/evt@v1.9.14/mod.ts";
+export { default as WS } from "https://deno.land/x/custom_socket@1.0.5/mod.ts";
